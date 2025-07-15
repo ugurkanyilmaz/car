44277348b65d288c7516088bbdb189dea590be63
Uğurkan Yılmaz <ugurkanyilmaz762@gmail.com>
2025-07-15 19:58:56 +0300

    start

---
+++ client/client.py
@@ -0,0 +1,5 @@
+# Entry point for client script
+# Will send CSV to server and generate Excel file
+
+if __name__ == "__main__":
+    pass
---
+++ server/server.py
@@ -0,0 +1,5 @@
+# Entry point for server script
+# Will provide REST API for CSV upload and resource merging
+
+if __name__ == "__main__":
+    pass
